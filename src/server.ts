8de3cfedb17acec87b5c5a12d2d12bed8995acb1
Chander <alexandertakepics@gmail.com>
2022-12-15 15:49:44 +0100

    database and first get method launch

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,14 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
 import cors from 'cors';
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (_req: Request, res: Response) => {
+    console.log('soy dislexico')
+    res.send('soy dislexico')
+})
+
+const PORT = 3000
+app.listen(PORT, () => console.log(`server running on port ${PORT}`))
