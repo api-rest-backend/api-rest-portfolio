2eefa29947f9d2345cc47f720db7e4dbb510e47e
Chander <alexandertakepics@gmail.com>
2022-12-20 13:43:45 +0100

    post done FINALLY

--- src/models/portfolio.model.ts
+++ src/models/portfolio.model.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 import { content } from './db.json'
 import fs from 'fs'
 
+const jsonContent = JSON.parse(fs.readFileSync('src/models/db.json', 'utf-8'))
 
 export const dataBase = {
     getData: () => {
@@ -14,31 +15,8 @@ export const dataBase = {
         return content.portfolio
     },
     postProject: (newProject) => {
-        // PUSH NEW PROJECT
-        console.log(newProject)
-        //content.portfolio.push(newProject)
-
-        //PARSE THE NEW DATA
-        const newParsed = JSON.parse(newProject)
-        console.log(newParsed)
-
-        //READ ALL THE CONTENT
-        fs.readFile('./src/models/db.json', 'utf-8', (error, data) => {
-            if (error) {
-                console.log(`ERROR: ${error}`)
-            } else {
-                const parsedContent = JSON.parse(data)
-                console.log(parsedContent)
-                // parsedContent.portfolio.push(newParsed)
-
-            }
-        })
-
-        // PUSH NEW CONTENT INTO EXISTING CONTENT
-
-
-        // WRITE NEW PROJECT 
-        // const addProject = fs.writeFileSync('./src/models/db.json', newProject)
-
+        jsonContent.content.portfolio.push(newProject)
+        console.log(jsonContent)
+        fs.writeFileSync('src/models/db.json', JSON.stringify(jsonContent, null, 4), 'utf-8')
     }
 }
