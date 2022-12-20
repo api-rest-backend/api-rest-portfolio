0722512649ccafede7ff7f87c730ae0773b93c2d
gbixio <galbixio93@gmail.com>
2022-12-20 13:46:50 +0100

    first try test

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,1 +1,14 @@
-test("2+2=4", () => expect(2 + 2).toBe(4));
+import request from "supertest";
+import app from '../src/app'
+
+// TESTING GET ENDPOINTS
+
+
+    it("return all information", (done) => {
+      request(app)
+        .get("/porfolio.mode.ts")
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200, done)
+    });
+  
